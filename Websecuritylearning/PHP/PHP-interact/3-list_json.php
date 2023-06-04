971495143c4dbaab97b7c42399175edbdbad17c0
Restonweb <2824398066@qq.com>
2023-06-04 18:47:35 +0800

    add python/comprehension.py

--- Websecuritylearning/PHP/PHP-interact/3-list_json.php
+++ Websecuritylearning/PHP/PHP-interact/3-list_json.php
@@ -1,7 +1,7 @@
 <?php
-    require_once('common.php');
+    require_once('3-common.php');
     $conn = create_connection();
-    $sql = "SELECT articleid,viewcount,createtime FROM article WHERE articleid < 30";
+    $sql = "SELECT articleid,author,headline,viewcount,createtime FROM article WHERE articleid < 30";
     $result = mysqli_query($conn,$sql);
     $data = mysqli_fetch_all($result,MYSQLI_ASSOC);
     echo json_encode($data);
